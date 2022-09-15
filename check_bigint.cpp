9893785e694c3a8afbb070dbedcabd578b95b379
Vinay <vinaysarda2812@gmail.com>
2022-09-15 11:00:09 +0530

    Big Integer in c++

---
+++ check_bigint.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+// #include "BigInteger.h"
+
+using namespace std;
+
+int main()
+{
+    BigInteger a, b, c;
+    a = "20";
+    b = "10";
+
+    cout << a / b;
+    return 0;
+}
